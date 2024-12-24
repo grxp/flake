0222b7d331091e97988867c2dc9d3928dca74899
grxp <grxp323@gmail.com>
2024-12-24 21:37:29 +0800

    package modify

--- packages/default.nix
+++ packages/default.nix
@@ -5,47 +5,25 @@
 {
   environment.systemPackages = with pkgs;
     [
-      # Download Tools
       wget
       axel
       curlHTTP3
-      # Editor & VCS
       git
       vim
       neovim
       nano
-      # Basic Tools
       htop
       tmux
-      # Devices
-      pciutils
-      usbutils
       smartmontools
-      # Network
-      inetutils
       dnsutils
       q
       dogdns
-      ethtool
       trippy
-      # Graphics
-      vulkan-tools
-      mesa-demos
-      # Video Codec
-      libva-utils
-      # Sensors
-      lm_sensors
-      # Rsync
-      rsync
-      # Compiler
       clang
       go
       rustup
       gcc
       gdb
-      # Runtime
-      nodejs
-      yarn
       python3
     ];
   programs.virt-manager.enable = true;
--- packages/gaming.nix
+++ packages/gaming.nix
@@ -2,26 +2,11 @@
 {
   programs.steam = {
     enable = true;
-    # gamescopeSession.enable = true;
-    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play, but i don't need
-    # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
   programs.gamescope.enable = true;
-  # programs.gamescope.capSysNice = true;
 
   programs.gamemode.enable = true;
-
-  # nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-  # programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-
-  # genshin impact global download accelerate
-  # ip via autopatchcn.yuanshen.com.wsdvs.com.  autopatchhk.yuanshen.com
-
-  # programs.anime-borb-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
-  # programs.honkers-launcher.enable = true;
-  # programs.wavey-launcher.enable = true;
-  # programs.sleepy-launcher.enable = true;
 }
