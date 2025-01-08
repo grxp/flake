9aacda8c17816f270397585d91126f9e043daf3c
DA <71159981+grxp@users.noreply.github.com>
2025-01-08 16:17:49 +0800

    feat: change network backend to iwd and disable powersave for wireless

--- specific/system-specific/Laptop8P/default.nix
+++ specific/system-specific/Laptop8P/default.nix
@@ -1,8 +1,11 @@
 {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  networking.networkmanager.wifi.powersave = false; # disabled power save mode for most driver to keep network stable
+  # networking.networkmanager.wifi.powersave = false; # disabled power save mode for most driver to keep network stable
   networking.networkmanager.wifi.backend = "iwd"; # Or Default:wpa_supplicant
+  networking.wireless.iwd.settings = {
+    DriverQuirks = { PowerSaveDisable = "mt7921e"; };
+  };
 
   networking.hostName = "Laptop8P"; # Define hostname.
 
