ecbb1d9fd25147dd9fee6f69e6b1a888c7df9264
grxp <71159981+grxp@users.noreply.github.com>
2025-04-10 20:08:07 +0800

    Game: enable other game of mihoyo hhh

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -18,10 +18,10 @@
   programs.gamemode.enable = true;
 
   nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-  # programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
+  programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
   programs.anime-games-launcher.enable = true;
-  # programs.honkers-railway-launcher.enable = true;
-  # programs.honkers-launcher.enable = true;
-  # programs.wavey-launcher.enable = true;
-  # programs.sleepy-launcher.enable = true;
+  programs.honkers-railway-launcher.enable = true;
+  programs.honkers-launcher.enable = true;
+  programs.wavey-launcher.enable = true;
+  programs.sleepy-launcher.enable = true;
 }
