9eb2c87fa0af0cdcc1f4974338de68e795942e8c
grxp <71159981+grxp@users.noreply.github.com>
2025-04-11 23:52:07 +0800

    Other: add group:wireshark to user

--- user/default.nix
+++ user/default.nix
@@ -8,6 +8,7 @@
     initialHashedPassword = "$y$j9T$pBWWxRVg6x9fa/1cZekGW1$hb.Vg3hwXSat17adx16GhwBdkJaQlKv.iib5pyjO/z1"; # set password directly
     extraGroups = [
       "networkmanager"
+      "wireshark"
       "wheel"
     ];
     packages = with pkgs; [
