c80abf752cb3e60090f877b4cf4699d64bc26606
grxp <71159981+grxp@users.noreply.github.com>
2025-09-29 23:37:23 +0800

    Feature: add fallback option for DE

--- specific/desktop/default.nix
+++ specific/desktop/default.nix
@@ -1,12 +1,26 @@
+{ lib, ... }:
 {
   imports = [
-    # ./Plasma.nix
     ./gnome
     ./pipewire.nix
   ];
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   # services.xserver.desktopManager.deepin.enable = true;
-  # services.desktopManager.plasma6.enable = true;
   services.flatpak.enable = true;
+
+  specialisation =
+    lib.mapAttrs
+      (name: config: {
+        configuration = {
+          system.nixos.tags = [ name ];
+          disabledModules = [ ./gnome ];
+        }
+        // config;
+      })
+      {
+        "Plasma" = {
+          imports = [ ./Plasma.nix ];
+        };
+      };
 }
