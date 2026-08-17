0de57852a2aeb509add03e54f36034ba01a404e0
grxp <71159981+grxp@users.noreply.github.com>
2026-08-18 00:35:53 +0800

    fix: flatpak font issue fixed

--- user/env.nix
+++ user/env.nix
@@ -2,6 +2,18 @@
 {
   xdg.dataFile."flatpak/overrides/global".text = ''
     [Context]
-    filesystems=/nix/store:ro;${config.xdg.dataHome}/fonts:ro;${config.home.homeDirectory}/.icons:ro
+    filesystems=/nix/store:ro;/home/darkair/.icons:ro;/home/darkair/.local/share/fonts:ro;host-etc:ro;
+
+    [Environment]
+    FONTCONFIG_FILE=/home/darkair/.config/fontconfig/fonts.conf
+  '';
+  xdg.configFile."fontconfig/fonts.conf".text = ''
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+    <fontconfig>
+     <description>Reuse the host (NixOS) fontconfig inside flatpak sandboxes</description>
+     <cachedir prefix="xdg">fontconfig</cachedir>
+     <include ignore_missing="yes">/run/host/etc/static/fonts/conf.d</include>
+    </fontconfig>
   '';
 }
